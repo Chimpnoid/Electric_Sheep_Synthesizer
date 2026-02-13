3525c97b22318add28e084730e0efb43bd8fb444
Jack Baines <jack.baines099@gmail.com>
2026-02-13 11:49:45 +0000

    Initial commit of files from c# project before makin any changes

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectricSheepSynth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
